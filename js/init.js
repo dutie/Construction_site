0e7a8bb725e26291d080e493fbce8452e8e58d22
dutie <r0695565@student.thomasmore.be>
2019-08-12 17:50:22 +0200

    More work to renovations. Added foto's and effects

--- js/init.js
+++ js/init.js
@@ -1,7 +1,18 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
 
+  });
+  $(window).scroll(function () {
+    $('#allImages div').each(function () {
+      console.log("hi, this image has padding of: " + $(this).offset().top);
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 400) {
+        $(this).addClass("slideUp");
+      }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
